08c2c66a51603fb4ba09edc7abdc916b744632a5
Yasmin Verderame <yasmin_verderame@hotmail.com>
2023-09-27 15:58:38 -0300

    adicionando projetos

---
+++ DESENVOLVIMENTO CROSS PLATFORM/Aula 1- flutter/lib/mocks/repositories_mock.dart
@@ -0,0 +1,121 @@
+import 'package:github/models/owner.dart';
+import 'package:github/models/repository.dart';
+
+final kRepositoriesMock = [
+  Repository(
+    id: 31792824,
+    name: "flutter",
+    owner: Owner(
+      login: "yasminverderame",
+      avatar: "https://avatars.githubusercontent.com/u/14101776?v=4",
+    ),
+    htmlUrl: "https://github.com/YasminVerderame/flutter",
+    description: "Aplicações para os cursos de pós-graduação da FIAP",
+    url: "https://api.github.com/users/flutter",
+  ),
+  Repository(
+    id: 14101776,
+    name: "flutter",
+    owner: Owner(
+      login: "flutter",
+      avatar: "https://api.github.com/users/flutter",
+    ),
+    htmlUrl: "https://github.com/flutter",
+    description: "Desafio 2 ~ FIAP",
+    url: "https://api.github.com/repos/maratonadev/desafio-2-2019",
+  ),
+  Repository(
+    id: 255656636,
+    name: "FIAP",
+    owner: Owner(
+      login: "diogenesjusto",
+      avatar: "https://avatars.githubusercontent.com/u/7620776?v=4",
+    ),
+    htmlUrl: "https://github.com/diogenesjusto/FIAP",
+    description: null,
+    url: "https://api.github.com/repos/diogenesjusto/FIAP",
+  ),
+  Repository(
+    id: 192782140,
+    name: "fiap-ml-visao-computacional",
+    owner: Owner(
+      login: "michelpf",
+      avatar: "https://avatars.githubusercontent.com/u/8541053?v=4",
+    ),
+    htmlUrl: "https://github.com/michelpf/fiap-ml-visao-computacional",
+    description:
+        "Repositório dos exemplos e desafios utilizados na disciplina de Visão Computacional do curso de MBA Machine Learning da FIAP",
+    url: "https://api.github.com/repos/michelpf/fiap-ml-visao-computacional",
+  ),
+  Repository(
+    id: 577429758,
+    name: "TurmaFiap",
+    owner: Owner(
+      login: "marcos-sabino-filho",
+      avatar: "https://avatars.githubusercontent.com/u/14055850?v=4",
+    ),
+    htmlUrl: "https://github.com/marcos-sabino-filho/TurmaFiap",
+    description: null,
+    url: "https://api.github.com/repos/marcos-sabino-filho/TurmaFiap",
+  ),
+  Repository(
+    id: 215315118,
+    name: "fiap-ml-visao-computacional-auditoria-video",
+    owner: Owner(
+      login: "michelpf",
+      avatar: "https://avatars.githubusercontent.com/u/8541053?v=4",
+    ),
+    htmlUrl:
+        "https://github.com/michelpf/fiap-ml-visao-computacional-auditoria-video",
+    description:
+        "Projeto final de conclusão da disciplina de Visão Computacional voltado a auditoria de vídeo e construção de classificadores baseado em transfer learning.",
+    url:
+        "https://api.github.com/repos/michelpf/fiap-ml-visao-computacional-auditoria-video",
+  ),
+  Repository(
+    id: 127837064,
+    name: "fiap-ml-prog-ia-python",
+    owner: Owner(
+      login: "michelpf",
+      avatar: "https://avatars.githubusercontent.com/u/8541053?v=4",
+    ),
+    htmlUrl: "https://github.com/michelpf/fiap-ml-prog-ia-python",
+    description:
+        "Aulas de programação de Python orientado a Machine Learning, com exemplos e desafios ao longo de 8 aulas.",
+    url: "https://api.github.com/repos/michelpf/fiap-ml-prog-ia-python",
+  ),
+  Repository(
+    id: 169169761,
+    name: "turma-fiap-3IA",
+    owner: Owner(
+      login: "pamelasantos",
+      avatar: "https://avatars.githubusercontent.com/u/7662015?v=4",
+    ),
+    htmlUrl: "https://github.com/pamelasantos/turma-fiap-3IA",
+    description: "Turma 3 IA - FIAP",
+    url: "https://api.github.com/repos/pamelasantos/turma-fiap-3IA",
+  ),
+  Repository(
+    id: 141432903,
+    name: "fiap-ml-tec-proc-imagens",
+    owner: Owner(
+      login: "michelpf",
+      avatar: "https://avatars.githubusercontent.com/u/8541053?v=4",
+    ),
+    htmlUrl: "https://github.com/michelpf/fiap-ml-tec-proc-imagens",
+    description:
+        "Repositório das aulas de tecnologia de processamento de imagens.",
+    url: "https://api.github.com/repos/michelpf/fiap-ml-tec-proc-imagens",
+  ),
+  Repository(
+    id: 470725194,
+    name: "FIAP-CICD-DevSecOps",
+    owner: Owner(
+      login: "vamperst",
+      avatar: "https://avatars.githubusercontent.com/u/2192283?v=4",
+    ),
+    htmlUrl: "https://github.com/vamperst/FIAP-CICD-DevSecOps",
+    description: null,
+    url: "https://api.github.com/repos/vamperst/FIAP-CICD-DevSecOps",
+  ),
+];
